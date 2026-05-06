ca93eddde2a0d85c2bf5dbc8ea4bfaff31b9ef6f
Joshua <jbe2262492@slu.edu.ph>
2026-05-06 11:10:51 +0800

    course class

--- .idea/src/Course.java
+++ .idea/src/Course.java
@@ -1,2 +1,30 @@
 public class Course {
+
+    String code;
+    String title;
+    double units;
+    String year;
+    String term;
+    String grade;
+
+    public Course(String code, String title, double units, String year, String term, String grade) {
+        this.code = code;
+        this.title = title;
+        this.units = units;
+        this.year = year;
+        this.term = term;
+        this.grade = grade;
+    }
+
+    public String toFileString() {
+        return code + "," + title + "," + units + "," + year + "," + term + "," + grade;
+    }
+
+    public double getNumericGrade() {
+        try {
+            return Double.parseDouble(grade);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
 }
